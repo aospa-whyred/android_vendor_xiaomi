de55a497b038e773cad4fc185228ed77525450ae
Aryan Gupta <guptaaryan189@gmail.com>
2020-01-29 08:56:49 +0000

    update vendor for wayne and jasmine

--- sdm660-common/Android.mk
+++ sdm660-common/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout wayne,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout wayne ,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := chargeonlymode
